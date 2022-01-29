68fdbc64bc237c9cf187ba93f337fd242e86172a
Buğrahan <bsaygici@hotmail.com>
2022-01-29 21:47:12 +0300

    RabbitMQ-Subscriber

---
+++ RabbitMQ-1-Subscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQ_1_Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
