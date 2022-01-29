a8a3a12d3bd770d61e835a950eca2eb07d275cf9
Buğrahan <bsaygici@hotmail.com>
2022-01-29 21:22:33 +0300

    Initial commit

---
+++ RabbitMQ-1-Publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQ_1_Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
