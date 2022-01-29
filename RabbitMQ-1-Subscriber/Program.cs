37894c0fca73780a9920e117c7a2c369c8b09132
Buğrahan <bsaygici@hotmail.com>
2022-01-30 01:09:32 +0300

    Added Subscriber Side

--- RabbitMQ-1-Subscriber/Program.cs
+++ RabbitMQ-1-Subscriber/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace RabbitMQ_1_Subscriber
 {
@@ -6,7 +9,28 @@ namespace RabbitMQ_1_Subscriber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://uaujyisy:H3i_uCahwKuNuShYsQa6TdDHfY4Cr6rF@tiger.rmq.cloudamqp.com/uaujyisy");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue", true, consumer);
+            
+            consumer.Received += (object sender, BasicDeliverEventArgs e)=>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Console.WriteLine("Coming Message: " + message);
+            };
+
+            Console.ReadLine();
         }
     }
 }
